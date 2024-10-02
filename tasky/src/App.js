e4c73ca9e595308a0d5cd963b1ba916440ba122f
Shane Power <20102592@mail.wit.ie>
2024-10-02 16:31:10 +0100

    Delete a task

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -17,6 +17,11 @@ function App() {
     setTaskState({tasks});
     console.log(`${taskIndex} ${tasks[taskIndex].done}`);
   }
+  const deleteHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks.splice(taskIndex, 1);
+    setTaskState({tasks});
+  } 
   return (
     <div className="container">
       <h1>Tasky</h1>
@@ -27,6 +32,7 @@ function App() {
       deadline={task.deadline}
       key={task.id}
       done={task.done}
+      deleteTask = {() => deleteHandler(index)}
       markDone={() => doneHandler(index)}
       
     />
